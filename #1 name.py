d9e5635b6816d5e4a50a3428a3460330ee89e8a1
shuq3 <154421897@qq.com>
2017-04-12 09:26:30 +0800

    1 and 2

---
+++ #1 name.py
@@ -0,0 +1,9 @@
+# learn the input and output of python
+# what you get from the input() is a string
+# str can add with another str and multiplicate with num
+
+name = input("Input your name:")
+age = input("Input your age:")
+
+# print中的"," 意味着一个空格
+print (name, "will be 100 years old at", 2117-int(age))
---
+++ #2 num.py
@@ -0,0 +1,15 @@
+# input if types int equality comparison numbers mod
+num = (int(input("Inuput a number:")))
+check = (int(input("Inuput another number:")))
+if num % 2 == 1:
+    print("It is a odd num")
+else:
+    print("It is a even num")
+
+if num % 4 == 0:
+    print("It is a mup of 4")
+
+if check == 0:
+    print ("The second number is zero")
+else:
+    print ("first num % second num is", num % check)
